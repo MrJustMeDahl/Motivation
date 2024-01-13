1d6cc591633c8dbf1b685cbe7f9296d9ef63808c
MrJustMeDahl <nico.dahl93@gmail.com>
2024-01-13 21:08:20 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">
+        <button>Click here to be motivated!</button>
+      </a>
+    </>
+  )
+}
+
+export default App
